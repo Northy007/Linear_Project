0d00372e417d7ddc25ae2710431d3184b0c8ef5b
northy <k.thaithat007@gmail.com>
2022-11-22 05:55:04 +0700

    new

--- display.py
+++ display.py
@@ -9,7 +9,7 @@ from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("TEmperature in Madrid - 2019-2021-Mean.csv")
 x_col = 'number'
-y_col = 'AVDMean'
+y_col = 'AVMean'
 a = data[x_col].values
 b = data[y_col].values
 
@@ -31,10 +31,13 @@ lm.score(x_test_poly, y)
 lm.predict([x_train_poly[0]])
 
 fig = plt.figure(figsize=(10,6))
-plt.scatter(X, y, color = 'Green', alpha=.3)
-plt.plot(X, lm.predict(poly_reg.fit_transform(X)),color = 'red', alpha = .5)
+plt.scatter(X, data['HighMean'], color = 'red', alpha=.3)
+plt.scatter(X, data['LowMean'], color = 'blue', alpha=.3)
+plt.scatter(X, data['AVMean'], color = 'Green', alpha=.3)
+#plt.scatter(X, y, color = 'Green', alpha=.3)
+#plt.plot(X, lm.predict(poly_reg.fit_transform(X)),color = 'red', alpha = .5)
 plt.title('Polynomial Regression')
-plt.xlabel(x_col)
-plt.ylabel(y_col)
+plt.ylabel("Temperatuure in Celsius")
+plt.xlabel("Number")
 plt.show()
 
