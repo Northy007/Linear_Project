dca95de748557def49b5f5b7e281c8a6be1fa0be
northy <k.thaithat007@gmail.com>
2022-11-21 21:49:51 +0700

    yeah

--- CompareDisplay.py
+++ CompareDisplay.py
@@ -7,16 +7,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
-data = pd.read_csv("TEmperature in Madrid - 2021.csv")
-data2 = pd.read_csv("TEmperature in Madrid - PredictHighTemp.csv")
+data = pd.read_csv("TEmperature in Madrid - 2019-2021-Mean.csv")
+data2 = pd.read_csv("TEmperature in Madrid - 2019-2021-MeanPredict.csv")
 
 x_col = 'number'
-y_col = 'HighTemp'
+y_col = 'LowMean'
 a = data[x_col].values
 b = data[y_col].values
 
 day = data2['number']
-temp = data2['high2021']
+temp = data2['lowPre']
 
 X = a
 y = b
